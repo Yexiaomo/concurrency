3fb91d029137d79c01ac08c092ddf281c1e9b709
Yexiaomo <markdownpad@163.com>
2021-01-25 22:10:21 +0800

    单例模式-枚举实现

---
+++ src/java/com/fxb/concurrency/annotations/singleton/SinglePatternExample3.java
@@ -0,0 +1,30 @@
+package com.fxb.concurrency.annotations.singleton;
+
+import com.fxb.concurrency.annotations.ThreadSafe;
+
+/**
+ * 单例模式：枚举实现-最推荐
+ */
+@ThreadSafe
+public class SinglePatternExample3 {
+    //私有化，外部只能通过静态方法获取对象
+    private SinglePatternExample3() {}
+
+    public static SinglePatternExample3 getInstance(){
+        return Singleton.INSTANCE.getSingleton();
+    }
+    private enum Singleton{
+        INSTANCE;
+        //如果打算自定义自己的方法，那么必须在enum实例序列的最后添加一个分号。
+        //而且 Java 要求必须先定义 enum 实例
+        private SinglePatternExample3 singleton;
+
+        //JVM保证这个方法绝对只被调用一次
+        Singleton(){
+            singleton = new SinglePatternExample3();
+        }
+        public SinglePatternExample3 getSingleton(){
+            return singleton;
+        }
+    }
+}
