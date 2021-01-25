f789a759a0aaa758b9f6939170e98b417f4aa3c2
Yexiaomo <markdownpad@163.com>
2021-01-25 22:11:39 +0800

    单例模式静态实现

--- src/java/com/fxb/concurrency/annotations/singleton/SinglePatternExample2.java
+++ src/java/com/fxb/concurrency/annotations/singleton/SinglePatternExample2.java
@@ -4,15 +4,17 @@ import com.fxb.concurrency.annotations.NotThreadSafe;
 import com.fxb.concurrency.annotations.ThreadSafe;
 
 /**
- * 单例模式：饿汉模式
- * TODO：单例实例在类加载的时候进行创建
+ * 单例模式：使用静态代码块的饿汉模式
  */
 @ThreadSafe
 public class SinglePatternExample2 {
     //私有化，外部只能通过静态方法获取对象
     private SinglePatternExample2() {}
     //单例对象
-    private static SinglePatternExample2 instance = new SinglePatternExample2();
+    private static SinglePatternExample2 instance = null;
+    static {
+        instance = new SinglePatternExample2();
+    }
     //静态的工厂方法，保证对象只能初始化一次
     public static SinglePatternExample2 getInstance() {
         return instance;
