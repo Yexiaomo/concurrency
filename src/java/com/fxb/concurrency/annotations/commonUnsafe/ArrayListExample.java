bdf041af6d3f8aa42fb95958119ea07fda0431b6
Yexiaomo <markdownpad@163.com>
2021-01-28 21:32:16 +0800

    List&Set&Map

---
+++ src/java/com/fxb/concurrency/annotations/commonUnsafe/ArrayListExample.java
@@ -0,0 +1,46 @@
+package com.fxb.concurrency.annotations.commonUnsafe;
+
+import com.fxb.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class ArrayListExample {
+    private final static int clientTotal = 5000;
+    private final static int threadTotal = 200;
+    private static List<Integer> arrayList = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int _i = i;
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    update(_i);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size : {}", arrayList.size());
+    }
+
+    private static void update(int _i) {
+        arrayList.add(_i);
+    }
+}
+
