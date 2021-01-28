9534f41901fefd8b7dac442bc037d809a4d6cfec
Yexiaomo <markdownpad@163.com>
2021-01-28 21:19:59 +0800

    DateTime

---
+++ src/java/com/fxb/concurrency/annotations/commonUnsafe/DateFormatExample3.java
@@ -0,0 +1,46 @@
+package com.fxb.concurrency.annotations.commonUnsafe;
+
+import com.fxb.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@ThreadSafe
+public class DateFormatExample3 {
+    private final static DateTimeFormatter datetimeFormat = DateTimeFormat.forPattern("yyyyMMdd");
+    private final static int clientTotal = 5000;
+    private final static int threadTotal = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int _i = i;
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    update(_i);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+    }
+
+    private static void update(int i) {
+        log.info("{}, {}", i, DateTime.parse("20210122", datetimeFormat).toDateTime());
+    }
+}
