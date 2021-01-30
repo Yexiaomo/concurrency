f69d00eb077657dc54d0316840a033a14e3635a3
Yexiaomo <markdownpad@163.com>
2021-01-30 17:55:02 +0800

    并发容器J.U.C

---
+++ src/java/com/fxb/concurrency/annotations/concurrent/CopyOnWriteArraySetExample.java
@@ -0,0 +1,44 @@
+package com.fxb.concurrency.annotations.concurrent;
+
+import com.fxb.concurrency.annotations.NotThreadSafe;
+import com.fxb.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.*;
+
+@Slf4j
+@ThreadSafe
+public class CopyOnWriteArraySetExample {
+    private final static int clientTotal = 5000;
+    private final static int threadTotal = 200;
+    private static Set<Integer> set = new CopyOnWriteArraySet<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int _i = i;
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    update(_i);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size : {}", set.size());
+    }
+
+    private static void update(int _i) {
+        set.add(_i);
+    }
+}
+
